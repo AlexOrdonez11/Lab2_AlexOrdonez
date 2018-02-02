c75322a26fcea9fd13e980056845c6ce4a553f05
Alex <ale11davi@unitec.edu>
2018-02-02 15:56:19 -0600

    terminado segundo ejercicio

--- Lab2_AlexOrdonez.cpp
+++ Lab2_AlexOrdonez.cpp
@@ -3,13 +3,31 @@
 #include <time.h>
 #include <math.h>
 using namespace std;
+#define PI 3.14159265 
 
 //Verifica si un numero es triangular 
 bool verificacionDelNumero(int);
 int GeneradorRandom();
 int VerificarTriangularAnterior(int);
-
+void Ejercicio1();
+void Ejercicio2();
 int main(){
+	int opcionmain;
+	cout<<"Menu Principal"<<endl;
+	cout<<"1.Ejercicio 1"<<endl;
+	cout<<"2.Ejercicio 2"<<endl;
+	cout<<"3.Ejercicio 3"<<endl;
+	cin>>opcionmain;
+	switch(opcionmain){
+		case 1:
+			Ejercicio1();
+			break;
+		case 2:
+			Ejercicio2()
+	}
+	return 0;
+}
+void Ejercicio1(){
 	char AnsofUser='s';
 	while(AnsofUser=='s'||AnsofUser=='S'){
 		cout<<"Menu:"<<endl;
@@ -48,8 +66,6 @@ int main(){
 		cout<<"Desea continuar [s/n]:";
 		cin>>AnsofUser;
 	}
-	
-	return 0;
 }
 bool verificacionDelNumero(int numeroAevaluar){
 	int acumuladordelwhile=0;
@@ -70,11 +86,11 @@ int VerificarTriangularAnterior(int numeroAevaluar){
 	int acumuladordelwhile=0;
 	int contador=0;
 	while(acumuladordelwhile<numeroAevaluar){
-		acumuladordelwhile=contador+(contador+1);
+		contador++;
+		acumuladordelwhile=contador;
 		if(acumuladordelwhile>=numeroAevaluar){
-			return acumuladordelwhile-(contador+1);
+			return acumuladordelwhile-contador;
 		}
-		contador++;	
 
 	}
 }
@@ -84,5 +100,32 @@ int GeneradorRandom(){
 	cout<<"el numero es:"<<random;
 	return random;
 }
+void Ejercicio2(){
+	float lado1;
+	float lado2;
+	float lado3;
 
+	cout<<"Ingrese las dimensiones del triangulo"<<endl;
+	cin>>lado1;
+	cin>>lado2;
+	cin>>lado3;
+	if(lado1+lado2<lado3||lado2+lado3<lado1||lado1+lado3=lado2){
+		cout<<"No se puede hacer un triangulo con estas dmensiones"<<endl;
+	}else{
+		float AnguloA= (acos(((lado2^2)+(lado3^2)-(lado1^2))/(2*lado2*lado3)))*180/PI;
+		float AnguloB= (acos(((lado1^2)+(lado3^2)-(lado2^2))/(2*lado1*lado3)))*180/PI;
+		float AnguloC= (acos(((lado1^2)+(lado2^2)-(lado3^3))/(2*lado1*lado2)))*180/PI;
+		cout<<"El angulo A :"<<AnguloA<<endl;
+		cout<<"El angulo B :"<<AnguloB<<endl;
+		cout<<"El angulo C :"<<AnguloC<<endl;
+		if(AnguloA==90||AnguloB==90||AnguloC==90){
+			cout<<"El triangulo es rectangulo"<<endl;
+		}else{
+			cout<<"El triangulo no es rectangulo"<<endl;
+		}
+		float semiperimetro=(lado1+lado2+lado3)/2;
+		float Area=sqrt(semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-lado3));
+		cout<<"El area del triangulo es :"<<Area<<endl;
+	}
+}
 
